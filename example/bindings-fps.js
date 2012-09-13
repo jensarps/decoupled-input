078753a38998ec21d288a2b7c527a287b9e416af
Jens Arps <mail@jensarps.de>
2012-09-13 15:43:12 +0200

    Multiple bindings for fps example

--- example/bindings-fps.js
+++ example/bindings-fps.js
@@ -1,4 +1,4 @@
-define(function(){
+define(function () {
 
   /* This is an example bindings file */
 
@@ -7,33 +7,65 @@ define(function(){
 
   var bindings = {
 
-    forward: {
-      device: KEYBOARD,
-      inputId: 87, // w
-      down: true,
-      up: true
-    },
+    forward: [
+      {
+        device: KEYBOARD,
+        inputId: 87, // w
+        down: true,
+        up: true
+      },
+      {
+        device: KEYBOARD,
+        inputId: 38, // cursor up
+        down: true,
+        up: true
+      }
+    ],
 
-    backward: {
-      device: KEYBOARD,
-      inputId: 83, // s
-      down: true,
-      up: true
-    },
+    backward: [
+      {
+        device: KEYBOARD,
+        inputId: 83, // s
+        down: true,
+        up: true
+      },
+      {
+        device: KEYBOARD,
+        inputId: 40, // cursor down
+        down: true,
+        up: true
+      }
+    ],
 
-    strafeLeft: {
-      device: KEYBOARD,
-      inputId: 65, // a
-      down: true,
-      up: true
-    },
+    strafeLeft: [
+      {
+        device: KEYBOARD,
+        inputId: 65, // a
+        down: true,
+        up: true
+      },
+      {
+        device: KEYBOARD,
+        inputId: 37, // cursor left
+        down: true,
+        up: true
+      }
+    ],
 
-    strafeRight: {
-      device: KEYBOARD,
-      inputId: 68, // d
-      down: true,
-      up: true
-    },
+    strafeRight: [
+      {
+        device: KEYBOARD,
+        inputId: 68, // d
+        down: true,
+        up: true
+      },
+      {
+        device: KEYBOARD,
+        inputId: 39, // cursor right
+        down: true,
+        up: true
+      }
+    ],
 
     turn: {
       device: MOUSE,
