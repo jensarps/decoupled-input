602d60cc15dd7012ca5695d3d321f7e03dc98fcf
Jens Arps <mail@jensarps.de>
2012-09-18 15:58:33 +0200

    Update build

--- build/input.js
+++ build/input.js
@@ -1,9 +1,5 @@
-
-
 var InputController = function (bindings) {
-  this.bindings = {};
   this.deviceHandlers = {};
-  this.input = {};
   this.setupBindings(bindings);
 };
 
@@ -16,9 +12,12 @@ InputController.prototype = {
   input: null,
 
   setupBindings: function (bindings) {
+    this.bindings = {};
+    this.input = {};
+
     Object.keys(bindings).forEach(function (description) {
       var binding = bindings[description],
-          toString = ({}).toString;
+        toString = ({}).toString;
 
       // set a default value; the value must be readable before
       // a user input occurs.
@@ -35,19 +34,29 @@ InputController.prototype = {
     }, this);
   },
 
+  updateBindings: function (bindings) {
+    this.setupBindings(bindings);
+
+    Object.keys(this.deviceHandlers).forEach(function (deviceName) {
+      this.deviceHandlers[deviceName].bindings = this.bindings[deviceName] || {};
+      this.deviceHandlers[deviceName].input = this.input;
+    }, this);
+  },
+
   _applyBinding: function (binding, description) {
     if (!this.bindings[binding.device]) {
       this.bindings[binding.device] = {};
     }
     this.bindings[binding.device][binding.inputId] = {
       description: description,
-      down: binding.down,
-      up: binding.up
+      down: !!binding.down,
+      up: !!binding.up,
+      invert: !!binding.invert
     }
   },
 
   registerDeviceHandler: function (DeviceHandler, deviceName) {
-    this.deviceHandlers[deviceName] = new DeviceHandler(this.bindings[deviceName], this.input);
+    this.deviceHandlers[deviceName] = new DeviceHandler(this.bindings[deviceName] || {}, this.input);
   },
 
   destroy: function () {
@@ -58,7 +67,184 @@ InputController.prototype = {
 
 };
 
-var KeyboardHandler = function(bindings, input){
+
+/**
+ * Gamepad polling code taken from this fantastic article by Marcin Wichary:
+ *
+ * http://www.html5rocks.com/en/tutorials/doodles/gamepad/
+ */
+
+var GamepadHandler = function (bindings, input) {
+  this.bindings = bindings;
+  this.input = input;
+
+  this.gamepads = [];
+  this.prevRawGamepadTypes = [];
+  this.prevTimestamps = [];
+  this.buttonStates = [];
+
+  this.init();
+};
+
+GamepadHandler.prototype = {
+
+  deadzone: 0.01,
+
+  buttonStates: null,
+
+  gamepads: null,
+
+  isPolling: false,
+
+  prevRawGamepadTypes: null,
+
+  prevTimestamps: null,
+
+  init: function () {
+
+    var isAvailable = !!navigator.webkitGetGamepads || !!navigator.webkitGamepads || (navigator.userAgent.indexOf('Firefox/') != -1);
+
+    if (isAvailable) {
+      window.addEventListener('MozGamepadConnected', ( this.connectListener = this.onGamepadConnect.bind(this) ), false);
+      window.addEventListener('MozGamepadDisconnected', ( this.disconnectListener = this.onGamepadDisconnect.bind(this) ), false);
+      if (!!navigator.webkitGamepads || !!navigator.webkitGetGamepads) {
+        this.startPolling();
+      }
+    }
+  },
+
+  onGamepadConnect: function (evt) {
+    this.gamepads.push(evt.gamepad);
+    this.startPolling();
+  },
+
+  onGamepadDisconnect: function (evt) {
+    for (var i = this.gamepads - 1; i >= 0; i--) {
+      if (this.gamepads[i].index == evt.gamepad.index) {
+        this.gamepads.splice(i, 1);
+        break;
+      }
+    }
+    if (this.gamepads.length == 0) {
+      this.stopPolling();
+    }
+  },
+
+  startPolling: function () {
+    if (!this.isPolling) {
+      this.isPolling = true;
+      this.tick();
+    }
+  },
+
+  stopPolling: function () {
+    this.isPolling = false;
+  },
+
+  tick: function () {
+    if (this.isPolling) {
+      var tick = this.tick.bind(this);
+      if (window.requestAnimationFrame) {
+        window.requestAnimationFrame(tick);
+      } else if (window.mozRequestAnimationFrame) {
+        window.mozRequestAnimationFrame(tick);
+      } else if (window.webkitRequestAnimationFrame) {
+        window.webkitRequestAnimationFrame(tick);
+      }
+    }
+
+    this.pollStatus();
+  },
+
+  pollStatus: function () {
+    this.pollGamepads();
+    for (var i = 0, m = this.gamepads.length; i < m; i++) {
+      var gamepad = this.gamepads[i];
+      if (gamepad.timestamp && (gamepad.timestamp == this.prevTimestamps[i])) {
+        continue;
+      }
+      this.prevTimestamps[i] = gamepad.timestamp;
+      this.onStatusChanged(i);
+    }
+  },
+
+  pollGamepads: function () {
+    var rawGamepads = (navigator.webkitGetGamepads && navigator.webkitGetGamepads()) || navigator.webkitGamepads;
+    var i, m;
+
+    if (rawGamepads) {
+      this.gamepads.length = 0;
+      var gamepadsChanged = false;
+      for (i = 0, m = rawGamepads.length; i < m; i++) {
+        if (typeof rawGamepads[i] != this.prevRawGamepadTypes[i]) {
+          gamepadsChanged = true;
+          this.prevRawGamepadTypes[i] = typeof rawGamepads[i];
+        }
+        if (rawGamepads[i]) {
+          this.gamepads.push(rawGamepads[i]);
+        }
+      }
+      if (gamepadsChanged) {
+
+        this.buttonStates.length = 0;
+
+        for (i = 0, m = this.gamepads.length; i < m; i++) {
+          var gamepad = this.gamepads[i];
+          var states = this.buttonStates[i] = [];
+          gamepad.buttons.forEach(function (initialValue, index) {
+            states[index] = initialValue;
+          }, this);
+        }
+
+      }
+    }
+  },
+
+  onStatusChanged: function (gamepadId) {
+
+    var gamepad = this.gamepads[gamepadId];
+    var states = this.buttonStates[gamepadId];
+
+    var i, m, binding;
+
+    for (i = 0, m = gamepad.buttons.length; i < m; i++) {
+      var currentValue = gamepad.buttons[i];
+      var oldValue = states[i];
+      var buttonId = 'button-' + i;
+
+      if (oldValue != currentValue) {
+        var type = currentValue > oldValue ? 'down' : 'up';
+        states[i] = currentValue;
+
+        if (buttonId in this.bindings) {
+          binding = this.bindings[buttonId];
+          if (binding[type]) {
+            this.input[binding.description] = currentValue;
+          }
+        }
+      }
+    }
+
+    for (i = 0, m = gamepad.axes.length; i < m; i++) {
+      var axisId = 'axis-' + i;
+      if (axisId in this.bindings) {
+        binding = this.bindings[axisId];
+        var value = gamepad.axes[i];
+        if (binding.invert) {
+          value *= -1;
+        }
+        this.input[binding.description] = Math.abs(value) > this.deadzone ? value : 0;
+      }
+    }
+  },
+
+  destroy: function () {
+    window.removeEventListener('MozGamepadConnected', this.connectListener, false);
+    window.removeEventListener('MozGamepadDisconnected', this.disconnectListener, false);
+  }
+};
+
+var KeyboardHandler = function (bindings, input) {
   this.bindings = bindings;
   this.input = input;
 
@@ -68,31 +254,32 @@ var KeyboardHandler = function(bindings, input){
 
 KeyboardHandler.prototype = {
 
-  onKeyDown: function(evt){
-    if(evt.keyCode in this.bindings){
+  onKeyDown: function (evt) {
+    if (evt.keyCode in this.bindings) {
       var binding = this.bindings[evt.keyCode];
-      if(binding.down){
+      if (binding.down) {
         this.input[binding.description] = 1;
       }
     }
   },
 
-  onKeyUp: function(evt){
-    if(evt.keyCode in this.bindings){
+  onKeyUp: function (evt) {
+    if (evt.keyCode in this.bindings) {
       var binding = this.bindings[evt.keyCode];
-      if(binding.up){
+      if (binding.up) {
         this.input[binding.description] = 0;
       }
     }
   },
 
-  destroy: function(){
+  destroy: function () {
     document.removeEventListener('keyup', this.upListener, false);
     document.removeEventListener('keydown', this.downListener, false);
   }
 };
 
-var MouseHandler = function(bindings, input){
+
+var MouseHandler = function (bindings, input) {
   this.bindings = bindings;
   this.input = input;
 
@@ -102,16 +289,14 @@ var MouseHandler = function(bindings, input){
   document.addEventListener('mousemove', ( this.moveListener = this.onMouseMove.bind(this) ), false);
   document.addEventListener('mousedown', ( this.downListener = this.onMouseDown.bind(this) ), false);
   document.addEventListener('mouseup', ( this.upListener = this.onMouseUp.bind(this) ), false);
-  document.addEventListener('contextmenu', ( this.ctxListener = function(evt){ evt.preventDefault(); }).bind(this), false);
+  document.addEventListener('contextmenu', ( this.ctxListener = function (evt) {
+    evt.preventDefault();
+  }).bind(this), false);
   window.addEventListener('resize', ( this.resizeListener = this.onResize.bind(this) ), false);
   this.onResize();
 };
 
 MouseHandler.prototype = {
-
-  invertXAxis: false,
-
-  invertYAxis: false,
 
   infiniteXAxis: false,
 
@@ -121,21 +306,18 @@ MouseHandler.prototype = {
 
   height: 0,
 
-  onMouseMove: function(evt){
+  onMouseMove: function (evt) {
+
     var x, y, mouseX, mouseY,
-        width = this.width,
-        halfWidth = width / 2,
-        height = this.height,
-        halfHeight = height / 2;
+      width = this.width,
+      halfWidth = width / 2,
+      height = this.height,
+      halfHeight = height / 2;
 
-    function clamp(min, max, value){
-      return Math.min(max, Math.max(min, value));
-    }
-
-    if(document.pointerLockEnabled){
-      mouseX = clamp(0, width, this.input.mouseX + evt.movementX);
-      mouseY = clamp(0, height, this.input.mouseY + evt.movementY);
-    }else{
+    if (document.pointerLockEnabled) {
+      mouseX = this.clamp(0, width, this.input.mouseX + evt.movementX);
+      mouseY = this.clamp(0, height, this.input.mouseY + evt.movementY);
+    } else {
       mouseX = evt.pageX;
       mouseY = evt.pageY;
     }
@@ -147,50 +329,47 @@ MouseHandler.prototype = {
       ( document.pointerLockEnabled ? evt.movementY : mouseY - this.input.mouseY ) :
       -( mouseY - halfHeight ) / halfHeight;
 
-    if(this.invertXAxis){
-      x *= -1;
-    }
-    if(this.invertYAxis){
-      y *= -1;
-    }
-
     this.input.mouseX = mouseX;
     this.input.mouseY = mouseY;
 
-    if('x' in this.bindings){
+    if ('x' in this.bindings) {
       var binding = this.bindings.x;
-      this.input[binding.description] = x;
+      this.input[binding.description] = binding.invert ? x * -1 : x;
     }
-    if('y' in this.bindings){
+    if ('y' in this.bindings) {
       var binding = this.bindings.y;
-      this.input[binding.description] = y;
+      this.input[binding.description] = binding.invert ? y * -1 : y;
     }
   },
 
-  onMouseDown: function(evt){
-    if(evt.button in this.bindings){
+  onMouseDown: function (evt) {
+    if (evt.button in this.bindings) {
       var binding = this.bindings[evt.button];
-      if(binding.down){
+      if (binding.down) {
         this.input[binding.description] = 1;
       }
     }
   },
 
-  onMouseUp: function(evt){
-    if(evt.button in this.bindings){
+  onMouseUp: function (evt) {
+    if (evt.button in this.bindings) {
       var binding = this.bindings[evt.button];
-      if(binding.up){
+      if (binding.up) {
         this.input[binding.description] = 0;
       }
     }
   },
 
-  onResize: function(){
+  onResize: function () {
     this.width = window.innerWidth;
     this.height = window.innerHeight;
   },
 
-  destroy: function(){
+  clamp: function (min, max, value) {
+    return Math.min(max, Math.max(min, value));
+  },
+
+  destroy: function () {
     document.removeEventListener('mousemove', this.moveListener, false);
     document.removeEventListener('mousedown', this.downListener, false);
     document.removeEventListener('mouseup', this.upListener, false);
