d64b4f49d01edf9a689ede2bf15d90e88bf1210c
Jens Arps <mail@jensarps.de>
2013-07-15 18:43:32 +0200

    Add gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+
+  // Project configuration.
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      all: ['src/*'],
+      options: {
+        bitwise: true,
+        camelcase: true,
+        curly: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        noempty: true,
+        nonew: true,
+        quotmark: true,
+        undef: true,
+        unused: true,
+        strict: true,
+        trailing: true,
+
+        loopfunc: true
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('default', ['jshint']);
+
+};
