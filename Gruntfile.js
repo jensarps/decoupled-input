9ff43c9d7867de1f4b57c2d6c2beb8816cfda225
Jens Arps <mail@jensarps.de>
2013-07-19 10:04:37 +0200

    Make missing handler module fatal

--- Gruntfile.js
+++ Gruntfile.js
@@ -103,7 +103,7 @@ module.exports = function (grunt) {
       for (var i = 0, m = handlerClassNames.length; i < m; i++) {
         var fileName = handlerClassNames[i] + '.js';
         if (!grunt.file.isFile('src/' + fileName)) {
-          grunt.log.error('ERROR: Device handler "' + fileName + '" doesn\'t exist.');
+          grunt.fatal('Device handler "' + fileName + '" doesn\'t exist.');
           return false;
         }
       }
